867954b94f9352702791946b6b43e83aa37612b3
sadri <alisadri1993@gmail.com>
2022-02-16 22:02:36 +0330

    some changes

---
+++ Store.FileManager/Service/MinIoFileService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.FileManager.Service
+{
+    internal class MinIoFileService : IFileService
+    {
+        public Task<FileInfo> GetFileByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SaveFileAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
